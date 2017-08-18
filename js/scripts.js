bf2bfc892910ae85b1a4ff761b8d2b898e231c61
Austin Blain <austin.a.blain@gmail.com>
2017-08-18 00:27:18 -0700

    add about section pages and services pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var imagesrc = 2;
+var timeAmount = 5000;
+
 $(document).ready(function() {
   $("#toggleMenu").click(function() {
     if ($(".mobileNav .navlist").hasClass("showing") === true) {
@@ -9,17 +12,17 @@ $(document).ready(function() {
     }
   });
 
-  // function slideshow(i) {
-  //   setTimeout(function(){
-  //     $("#slideShow img").attr("src","images/slideshow/" + imagesrc + ".jpg");
-  //     if (imagesrc < 6) {
-  //       imagesrc++;
-  //     } else if (imagesrc >= 6) {
-  //       imagesrc = 1;
-  //     }
-  //     slideshow(imagesrc);
-  //   }, timeAmount);
-  // };
-  //
-  // slideshow(imagesrc);
+  function slideshow(i) {
+    setTimeout(function(){
+      $("#slideshow").attr("src","images/slideshow/" + imagesrc + ".jpg");
+      if (imagesrc < 6) {
+        imagesrc++;
+      } else if (imagesrc >= 6) {
+        imagesrc = 1;
+      }
+      slideshow(imagesrc);
+    }, timeAmount);
+  };
+
+  slideshow(imagesrc);
 });
